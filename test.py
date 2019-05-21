c9367dea51145e07958aa00d8f8dc32b47f19692
paduel <paduel@gmail.com>
2019-05-21 11:42:10 +0200

    Update

---
+++ test.py
@@ -0,0 +1,12 @@
+_zmq = DWX_ZeroMQ_Connector()
+
+
+
+_zmq._DWX_MTX_CLOSE_ALL_TRADES_()
+
+#
+# _my_trade = _zmq._generate_default_order_dict()
+# _zmq._DWX_MTX_NEW_TRADE_(_order=_my_trade)
+
+
+# _zmq._DWX_MTX_SUBSCRIBE_MARKETDATA_('EURUSD')
